2ef4bbb9afe6e894e02fd899b1aa0930d1737fbd
soar <soar.lin@gmail.com>
2015-01-14 08:52:06 +0800

    first commit

---
+++ app/views/emails/contactemail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <h2>Contact Form</h2>
+  <div>Someone just contacted us:</div>
+  <div>Subject: {{ $subject }}</div>
+  <div>Message: {{ $emailmessage }}</div>
+</body>
+</html>
---
+++ app/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="zh-TW">
+<head>
+  <meta charset="UTF-8">
+  <title>另北的Laravel測試</title>
+</head>
+<body>
+  <ul>
+    <li><a href="./">Home</a></li>
+    <li><a href="./about">About</a></li>
+    <li><a href="./contact">Contact</a></li>
+  </ul>
+@yield('content')
+</body>
+</html>
