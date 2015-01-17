6fd8fa428850d0c991b3c0347d0d059bb542c6d3
soar <soar.lin@gmail.com>
2015-01-17 14:23:09 +0800

    [Add]bower init file and re-layout by bootstrap

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layout')
 @section('content')
-  <h1>Welcome!</h1>
-  <p>Learning Laravel is a learning center. You can find everything about Laravel here.</p>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <h1>Welcome!</h1>
+      <p>Learning Laravel is a learning center. You can find everything about Laravel here.</p>
+    </div>
+  </div>
+</div>
 @stop
